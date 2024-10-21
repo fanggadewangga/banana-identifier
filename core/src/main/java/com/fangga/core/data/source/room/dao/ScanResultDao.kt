978b8caea534c54c3647f7cb536fd2101f599a77
Fa'iq Arya Dewangga <fanggadewangga@gmail.com>
2024-10-21 21:49:31 +0700

    feat(result): change get saved result query

--- core/src/main/java/com/fangga/core/data/source/room/dao/ScanResultDao.kt
+++ core/src/main/java/com/fangga/core/data/source/room/dao/ScanResultDao.kt
@@ -5,14 +5,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.fangga.core.data.source.room.entity.ScanResultEntity
+import com.fangga.core.utils.Constants.LATEST_RESULT_ID
 
 @Dao
 interface ScanResultDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNewScanResult(scanResultEntity: ScanResultEntity)
 
-    @Query("SELECT * FROM ScanResultEntity")
-    suspend fun getAllScanResults(): List<ScanResultEntity>
+    @Query("SELECT * FROM ScanResultEntity WHERE resultId != :latestResultId")
+    suspend fun getAllScanResults(latestResultId: String = LATEST_RESULT_ID): List<ScanResultEntity>
 
     @Query("SELECT * FROM ScanResultEntity WHERE resultId = :resultId")
     suspend fun getScanResultById(resultId: String): ScanResultEntity?
